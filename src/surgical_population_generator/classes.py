5b9fe62e1ab227543462c358cddf1599a1245e1a
pontus-p <pfannenstill@gmail.com>
2026-02-27 13:20:34 +0100

    start

--- src/surgical_population_generator/classes.py
+++ src/surgical_population_generator/classes.py
@@ -9,3 +9,14 @@ class Diagnosis:
     LoS: float
     male_percent: int
     age_distribution: dict
+
+    def __post_init__(self):
+        del self.age_distribution["NA"]
+        self.male_probability = self.male_percent / 100
+
+    def normalized_age_distribution(self):
+        total_occurances = sum(v for v in self.age_distribution.values())
+        ret = {}
+        for k, v in self.age_distribution.items():
+            ret[k] = v/total_occurances
+        return ret
--- src/surgical_population_generator/generator.py
+++ src/surgical_population_generator/generator.py
@@ -1,8 +1,42 @@
 from classes import Diagnosis
+from pprint import pprint
+import random
 from distributions.distributions_data import diagnosis_distributions
+
+def generate_diagnosis_probabilities(diags: list):
+    sum_individuals = sum(diag.occurances for diag in diags)
+    ret = {}
+    for diag in diags:
+        ret[diag.ICD] = diag.occurances / sum_individuals
+    return ret
+
+def generate_population(n: int, diagnosis_probabilities: dict, random_sample=False):
+    """Generates individuals in a list given a probability distribution.
+    Result follow the distribution deterministically.
+    Rounding errors in small sets are added in based on the probability distribution.
+    If random_sample is True, all individuals are sampled randomly.
+    """
+    diags = list(diagnosis_probabilities.keys())
+    probs = list(diagnosis_probabilities.values())
+    if random_sample:
+        return random.choices(diags, weights=probs, k=n)
+
+    distributed_diagnosis_list = []
+    for diag, p in diagnosis_probabilities.items():
+        for _ in range(int(p*n)):
+            distributed_diagnosis_list.append(diag)
+    patients_to_add_in = n - len(distributed_diagnosis_list)
+    distributed_diagnosis_list.extend(random.choices(diags, weights=probs, k=patients_to_add_in))
+    assert n == len(distributed_diagnosis_list)
+    return distributed_diagnosis_list
+
 
 diags = []
 for k, v in diagnosis_distributions.items():
     diags.append(Diagnosis(**v))
 
-print(diags)
+# pprint(generate_diagnosis_probabilities(diags))
+diagnosis_probabilities = generate_diagnosis_probabilities(diags)
+x = (generate_population(10, diagnosis_probabilities, random_sample=False))
+print(len(x))
+
